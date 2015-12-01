1691f901f2f5e0be69d5e18d6a27acd8021acb74
Miles France <miles.france@gmail.com>
2015-11-30 23:02:36 -0700

    added front page - enable zap initialize last

---
+++ modules/custom/zap_initialize/src/Controller/ZapController.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\zap_initialize\Controller\ZapController.
+ */
+
+namespace Drupal\zap_initialize\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Render\RendererInterface;
+
+/**
+ * Controller routines for zap routes.
+ */
+class ZapController extends ControllerBase {
+
+  /**
+   * Routes for zap.
+   *
+   * @return array
+   *   A render array representing the zap page content.
+   *
+   */
+  public function zapFront() {
+    return array();
+  }
+}
