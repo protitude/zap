3d0dc190a10e6da1739b42f0c6657acd145acdd7
protitude <miles.france@gmail.com>
2016-06-08 11:15:25 -0600

    backing up file locations

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -2,6 +2,6 @@
 
 rm -fR core
 echo 'Removing core from composer build'
-cp scripts/settings.php ../../sites/default/
-chmod 0666 ../../sites/default/settings.php
+cp scripts/settings.php ../../../sites/default/
+chmod 0666 ../../../sites/default/settings.php
 
