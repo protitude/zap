f3077e2be1b30932647e894f45060674b9ac2135
protitude <miles.france@gmail.com>
2016-06-07 19:26:58 -0600

    adding settings.php

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -2,4 +2,6 @@
 
 rm -fR core
 echo 'Removing core from composer build'
+cp scripts/settings.php ../../sites/default/
+chmod 0666 ../../sites/default/settings.php
 
---
+++ scripts/settings.php
@@ -0,0 +1,24 @@
+<?php
+
+$databases = array();
+$config_directories = array();
+$settings['hash_salt'] = '';
+$settings['update_free_access'] = FALSE;
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+$settings['trusted_host_patterns'] = array(
+  '^drupalvm\.dev',
+);
+$settings['hash_salt'] = 'rXRqLDFU2Ls4oCohqGu9YX_SrbvT_2cjcCAKqOQqFps';
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\Core\Database\Driver\mysql',
+  'driver' => 'mysql',
+);
+$settings['install_profile'] = 'zap';
+$config_directories['sync'] = 'sites/default/files/config_4eJmMXr6BeZgx7dcsXSb9SDTZ6HzGJnNpyJia3ymYnLKtS_xdLdg_f3A4GB2HNJDg3BWE2ODNQ/sync';
+
