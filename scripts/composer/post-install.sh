d48b148857ad60de7d8f0155e0e0badfb0cedcc4
protitude <miles.france@gmail.com>
2016-06-14 22:02:15 -0600

    added pattern lab to install - run npm install for theme

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -4,4 +4,5 @@ rm -fR core
 echo 'Removing core from composer build'
 cp scripts/settings.php ../../../sites/default/
 chmod 0666 ../../../sites/default/settings.php
+npm install --prefix ./themes/contrib/patternlab
 
