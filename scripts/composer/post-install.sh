3e51b3403a81638b02f3505c78496a8e21e6314e
protitude <miles.france@gmail.com>
2016-06-15 13:11:17 -0600

    adding settings.local.php with development settings

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -4,5 +4,7 @@ rm -fR core
 echo 'Removing core from composer build'
 cp scripts/settings.php ../../../sites/default/
 chmod 0666 ../../../sites/default/settings.php
+cp scripts/development.services.yml ../../../sites/
+cp scripts/example.settings.local.php ../../../sites/settings.local.php
 npm install --prefix ./themes/contrib/patternlab
 
