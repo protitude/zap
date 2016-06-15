d4e7b24b4af3d078931c5c7802a3abc6a54faa49
protitude <miles.france@gmail.com>
2016-06-15 16:39:24 -0600

    update settings.local copy script

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -5,6 +5,6 @@ echo 'Removing core from composer build'
 cp scripts/settings.php ../../../sites/default/
 chmod 0666 ../../../sites/default/settings.php
 cp scripts/development.services.yml ../../../sites/
-cp scripts/example.settings.local.php ../../../sites/settings.local.php
+cp ../../../sites/example.settings.local.php ../../../sites/default/settings.local.php
 npm install --prefix ./themes/contrib/patternlab
 
